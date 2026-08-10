6eb4e6cb5c487fc959d8f8b24d2d79a98d5aa17a
jaimanbhati9-glitch <jaimanbhati9@gmail.com>
2026-08-10 12:41:11 +0530

    feat: setup basic HTTP server using core http module

--- main.js
+++ main.js
@@ -10,8 +10,20 @@
 // const filePath = path.join(__dirname, 'data','data.txt');
 // console.log(filePath);
 
-const process = require('process');
-console.log(process.argv);
-
+const http = require('http');
 require('dotenv').config();
-console.log(process.env.PORT);
+
+const server = http.createServer((req,res) => {
+    console.log(`request recieved from the url : ${req.url}`);
+    
+    res.statusCode = 200;
+    res.setHeader('Content-Type ', 'text/plain');
+    res.end('Hello from your Node-JS HTTP Server!');
+});
+
+
+
+const PORT = Number(process.env.PORT) || 3000;
+server.listen(PORT , () => {
+    console.log(`server is running at local host : ${PORT}`);
+});
